3981b639d03abab12b697419b2c0feb2b7fbc6ca
Anouar BEN ZAHRA <33717910+AnouarBenZahra@users.noreply.github.com>
2020-07-18 18:04:10 +0200

    add project

---
+++ MLBinaryClassificationTitanic/PredictionTitanic.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.ML.Runtime.Api;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MLBinaryClassificationTitanic
+{
+   public class PredictionTitanic
+    {
+        [ColumnName("PredictedLabel")]
+        public bool Prediction;
+
+        public float Score;
+    }
+}
